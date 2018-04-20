8052369a1e64ba291bf81ed1cdabe1861549b2c6
Cesar Caceres <ccaceres@ingenia.io>
2018-04-20 17:36:22 -0400

    adding licenses service

---
+++ licensing-service/src/main/java/com/cesar/example/sms/licenses/Servcap2Application.java
@@ -0,0 +1,13 @@
+package com.cesar.example.sms.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Servcap2Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(Servcap2Application.class, args);
+	}
+}
---
+++ licensing-service/src/main/java/com/cesar/example/sms/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,24 @@
+package com.cesar.example.sms.licenses.controllers;
+
+
+import com.cesar.example.sms.licenses.License;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public License getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return new License()
+                .withId(licenseId)
+                .withProductName("Teleco")
+                .withLicenseType("Seat")
+                .withOrganizationId("TestOrg");
+    }
+}
